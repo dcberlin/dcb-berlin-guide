eaccef743d1d750071bdef900bd6cc2e2d13598c
Theo Costea <theo.costea@gmail.com>
2021-06-12 14:10:16 +0200

    Read secret key from config

--- backend/src/berlin_guide/settings.py
+++ backend/src/berlin_guide/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "!v&030p8!4oncf#%fv-zc%l3b%3gjh0%8^d%s*z4)z%phd*h^z"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "true"

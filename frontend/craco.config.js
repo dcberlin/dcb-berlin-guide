cf626e23f521ec8b38d5b86b2f9bdd444290f275
Theo Costea <theo.costea@gmail.com>
2021-06-12 12:37:26 +0200

    Fix Mapbox empty tile in prod issue

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   style: {
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
-}
+  babel: {
+    loaderOptions: {
+      ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"],
+    },
+  },
+};
